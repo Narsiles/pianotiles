862e6d9fb3119ecda20acd662880ef75728cd110
Narsiles <92451486+Narsiles@users.noreply.github.com>
2021-12-16 00:46:11 +0100

    png

---
+++ src/tableau1.js
@@ -0,0 +1,26 @@
+class Tableau1 extends Phaser.Scene{
+
+    /**
+     * Précharge les assets
+     */
+    preload(){
+
+    }
+
+
+    /**
+     * Crée la scène
+     */
+    create(){
+        //initialise les écoutes de touches pressées et relâchées
+        this.initKeyboard();
+    }
+
+    initKeyboard(){
+
+    }
+
+    update(){
+
+    }
+}
